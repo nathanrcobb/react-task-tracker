e2fb23cb4cff10519474f62edbd2491b1140dbf4
Nathan Cobb <nathanrcobb@gmail.com>
2021-12-27 23:10:35 -0800

    Finished Project

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,13 +1,18 @@
-import Task from './Task'
+import Task from "./Task";
 
-const Tasks = ({ tasks }) => {
-    return (
-        <>
-            {tasks.map((task) => (
-                <Task key={task.id} task={task} text="Hello" />
-            ))}
-        </>
-    )
-}
+const Tasks = ({ tasks, onDelete, onToggle }) => {
+  return (
+    <>
+      {tasks.map((task) => (
+        <Task
+          key={task.id}
+          task={task}
+          onDelete={onDelete}
+          onToggle={onToggle}
+        />
+      ))}
+    </>
+  );
+};
 
-export default Tasks
+export default Tasks;
